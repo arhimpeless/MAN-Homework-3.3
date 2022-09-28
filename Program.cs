f45932441317cb5838b644b5cc2a7a3815a927aa
Andrey Markov <arherower@outlook.com>
2022-09-28 10:11:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Задание_3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуй, Искатель!\nВведи положительное число, а я выведу ближайшее простое число на консоль.");
+            while (true)
+            {
+                int neprostoe;
+                int nepr_mal = 0;
+                int nepr_bol = 0;
+                Console.Write("  Вводить число здесь: ");
+                neprostoe = int.Parse(Console.ReadLine());
+                while (true)
+                {
+                    if (neprostoe == 0 || neprostoe < 0)
+                    {
+                        Console.WriteLine("Я работаю только с положительными числами.");
+                        break;
+                    }
+                    if (neprostoe > 0)
+                    {
+                        if (IsPrimeNumber(neprostoe))
+                        {
+                            Console.WriteLine("Введено простое число: " + neprostoe + "\n");
+                            break;
+                        }
+                        for (int i = 1; i < neprostoe; i++)
+                        {
+                            if (IsPrimeNumber(i))
+                            {
+                                nepr_mal = i;
+                            }
+                        }
+                        for (int j = neprostoe + 1; ; j++)
+                        {
+                            if (IsPrimeNumber(j))
+                            {
+                                nepr_bol = j;
+                                break;
+                            }
+                        }
+                        if ((neprostoe - nepr_mal) < (nepr_bol - neprostoe))
+                        {
+                            Console.WriteLine("Ближайшее просто число меньше введённого: " + nepr_mal + "\n");
+                            break;
+                        }
+                        if ((neprostoe - nepr_mal) > (nepr_bol - neprostoe))
+                        {
+                            Console.WriteLine("Ближайшее простое число больше введённого: " + nepr_bol + "\n");
+                            break;
+                        }
+                        if ((neprostoe - nepr_mal) == (nepr_bol - neprostoe))
+                        {
+                            Console.WriteLine("Ближайших чисел два: " + nepr_bol + " " + nepr_mal + "\n");
+                            break;
+                        }
+                        break;
+                    }
+                    break;
+                }
+            }
+        }
+        static bool IsPrimeNumber(int neprostoe)
+        {
+            for (int i = 2; i < neprostoe; i++)
+            {
+                if (neprostoe % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
